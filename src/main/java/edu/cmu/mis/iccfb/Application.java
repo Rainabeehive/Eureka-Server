c7f45bb58d87d6ef64be9545b69c1073d6ec37e9
Rainabeehive <yuhanz@andrew.cmu.edu>
2017-02-25 23:31:45 -0500

    #1

--- src/main/java/edu/cmu/mis/iccfb/Application.java
+++ src/main/java/edu/cmu/mis/iccfb/Application.java
@@ -1,15 +1,31 @@
 package edu.cmu.mis.iccfb;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 @EnableEurekaServer
 public class Application {
+    
+    
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+  
+  @Autowired
+  private EurekaClient discoverClient;
+  
+  
+  
 }
